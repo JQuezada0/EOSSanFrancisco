f87452ed3637f95d7bf4f951e77b4675fb422201
JQuezada0 <johnil@blissfully.com>
2018-11-11 09:00:12 -0800

    Commit more

--- services/frontend/src/components/Home.js
+++ services/frontend/src/components/Home.js
@@ -139,11 +139,11 @@ const renderDropsFeed = function () {
         <Paper>
           <CommentBox
             title={asset.displayName}
-            
+
             body='is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in'
             city='New York'
             hour='3H'
-            
+
           />
         </Paper>
       ))
